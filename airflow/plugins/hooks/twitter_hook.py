6b808ce74bc5ef802fd8c811229cf70d4ba05bac
Renan Luiz Babinski <renan10babinski@hotmail.com>
2021-07-08 15:29:11 -0300

    hokks

---
+++ airflow/plugins/hooks/twitter_hook.py
@@ -0,0 +1,11 @@
+from /home/anonymous/datapipeline/.env/lib/python3.8/site-packages/airflow/hooks.
+
+class TwitterHook(HttpHook):
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+    
